abeb3fb1c475e22bf632348de6da3a028d3f8c12
Isaac Northrop <isaac.northrop88@gmail.com>
2023-12-28 10:38:48 -0800

    switch to array

--- main.c
+++ main.c
@@ -8,8 +8,6 @@
 struct Node
 {
     int value;
-    struct Node *next;
-    struct Node *prev;
     bool isEdit;
 };
 
@@ -22,80 +20,35 @@ struct Node *initNode(int value, bool isEdit)
         exit(EXIT_FAILURE);
     }
     new->value = value;
-    new->next = NULL;
-    new->prev = NULL;
     new->isEdit = isEdit;
     return new;
 }
 
-struct Node *listify(char *puzzle)
+void getArray(char *puzzle, struct Node sudoku[MAX_LENGTH])
 {
-    struct Node *head = NULL;
-
-    for (int i = MAX_LENGTH - 1; i >= 0; i--)
+    for (int i = 0; i < MAX_LENGTH; i++)
     {
-        struct Node *new;
         int val = puzzle[i] - '0';
         if (val == 0)
         {
-            new = initNode(val, true);
-        }
-        else if (val < 0)
-        {
-            fprintf(stderr, "Invalid entry.\n");
-            exit(EXIT_FAILURE);
+            sudoku[i].value = val;
+            sudoku[i].isEdit = false;
         }
         else
         {
-            new = initNode(val, false);
+            sudoku[i].value = val;
+            sudoku[i].isEdit = true;
         }
-
-        new->next = head;
-        if(head != NULL){
-            head->prev = new;
-        }
-        head = new;
     }
-
-    return head;
 }
 
-void printList(struct Node **head)
+void printSudoku(struct Node sudoku[MAX_LENGTH])
 {
-    struct Node *curr = NULL;
-    curr = *head;
-    while(curr != NULL){
-        printf("%d", curr->value);
-        curr = curr->next;
-    }
-    printf("\n");
-}
-
-void printSudoku(struct Node **head)
-{
-    struct Node *curr = NULL;
-    curr = *head;
-    for (int j = 0; j < MAX_LENGTH / 9; j++)
+    for (int i = 0; i < MAX_LENGTH; i++)
     {
-        for (int i = 0; i < MAX_LENGTH / 9; i++)
-        {
-            printf("%d ", curr->value);
-            curr = curr->next;
-        }
-        printf("\n");
-    }
-}
-
-void solve(struct Node **head)
-{
-    struct Node *curr = NULL;
-    curr = *head;
-    while(curr != NULL){
-        if(curr->prev == NULL && curr->value == 9){
-            printf("fuck\n");
-            return;
-        }
-        curr = curr->next;
+        printf("%d ", sudoku[i].value);
+        if((i+1)%9 == 0)
+            printf("\n");
     }
 }
 
@@ -115,13 +68,9 @@ int main()
         printf("You entered: %s\n", puzzle);
     }
 
-    struct Node *head = listify(puzzle);
-    printList(&head);
-    printf("Starting Sudoku:\n");
-    printSudoku(&head);
-    solve(&head);
-    printf("Solved Sudoku:\n");
-    printSudoku(&head);
+    struct Node sudoku[MAX_LENGTH];
+    getArray(puzzle, sudoku);
+    printSudoku(sudoku);
 
     return 0;
 }
